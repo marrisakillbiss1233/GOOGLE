bee839817ac574c60d8d6975b5a29a6243103ab4
marrisakillbiss1233 <marrisabrown@protonmail.com>
2025-06-13 13:52:01 -0400

    text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Google Account has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1 8555927132 to unlock it!';
+defaultText = 'Your Google Account has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1 8555745719 to unlock it!';
